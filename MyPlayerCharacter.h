095cefb98012ecc26d542f24d70c0974e43405fd
JangJinWoo <aktsksqkq@gmail.com>
2024-04-17 21:28:50 +0900

    기본 세팅

---
+++ MyPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/MyCharacterBase.h"
+#include "MyPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API AMyPlayerCharacter : public AMyCharacterBase
+{
+	GENERATED_BODY()
+	
+};
